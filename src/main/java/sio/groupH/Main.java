e8b452b3a8992be4402b1034b03091bed0f2c8f9
Hugo Huart <hugo.huart@heig-vd.ch>
2023-10-12 14:46:30 +0200

    heuristics: nn done

--- src/main/java/sio/groupH/Main.java
+++ src/main/java/sio/groupH/Main.java
@@ -1,9 +1,15 @@
 package sio.groupH;
 
+import sio.tsp.TspData;
+import sio.tsp.TspTour;
+
 public final class Main {
+  private String getStats(TspTour tour) {
+    return "";
+  }
+
   public static void main(String[] args) {
     // TODO
-    //  - Renommage du package ;
     //  - Implémentation des classes NearestNeighbor et DoubleEndsNearestNeighbor ;
     //  - Affichage des statistiques dans la classe Main ;
     //  - Documentation abondante des classes comprenant :
@@ -20,5 +26,25 @@ public final class Main {
 
     // Exemple de lecture d'un jeu de données :
     // TspData data = TspData.fromFile("data/att532.dat");
+
+    String[] dataFiles = new String[] {
+        "data/att532.dat",
+        "data/rat575.dat",
+        "data/rl1889.dat",
+        "data/u574.dat",
+        "data/u1817.dat",
+        "data/vm1748.dat"
+    };
+
+    try {
+      for (String file : dataFiles) {
+        TspData data = TspData.fromFile(file);
+
+        TspTour tour  = new NearestNeighbor().computeTour(data, 0);
+        System.out.printf("%s -> NN: %d\n", file, tour.length());
+      }
+    } catch(Exception e) {
+      System.out.println(e.getMessage());
+    }
   }
 }
--- src/main/java/sio/groupH/NearestNeighbor.java
+++ src/main/java/sio/groupH/NearestNeighbor.java
@@ -4,10 +4,60 @@ import sio.tsp.TspData;
 import sio.tsp.TspConstructiveHeuristic;
 import sio.tsp.TspTour;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.stream.IntStream;
+
 public final class NearestNeighbor implements TspConstructiveHeuristic {
+  private final int DEFAULT_SEARCH_INDEX = -1;
+
+  private int findClosestAvailableCity(TspData data, boolean[] isAvailable, int originCity) {
+    int closestCity = DEFAULT_SEARCH_INDEX;
+    long closestDistance = Long.MAX_VALUE;
+    long currentDistance;
+
+    for (int nextCity = 0; nextCity < data.getNumberOfCities(); ++nextCity) {
+      currentDistance = data.getDistance(originCity, nextCity);
+      if (currentDistance < closestDistance && isAvailable[nextCity]) {
+        closestCity = nextCity;
+        closestDistance = currentDistance;
+      }
+    }
+
+    return closestCity;
+  }
+
   @Override
   public TspTour computeTour(TspData data, int startCityIndex) {
-    // TODO
-    return null;
+    if (data.getNumberOfCities() < 3) {
+      throw new IllegalArgumentException("There should be at least 3 cities");
+    }
+
+    if (startCityIndex < 0 || startCityIndex >= data.getNumberOfCities()) {
+      throw new IllegalArgumentException("Start city index should be contained in [0, Number of cities[");
+    }
+
+    int[] tour = new int[data.getNumberOfCities()];
+    boolean[] isAvailable = new boolean[data.getNumberOfCities()];
+    Arrays.fill(isAvailable, true);
+
+    long totalDistance = 0;
+    int current = startCityIndex;
+    int next;
+
+    for (int i = 0; i < data.getNumberOfCities(); ++i) {
+      tour[i] = current;
+      isAvailable[current] = false;
+
+      next = findClosestAvailableCity(data, isAvailable, current);
+      if (next != DEFAULT_SEARCH_INDEX) {
+        // Si on n'est pas dans le cas de la dernière visitée
+        totalDistance += data.getDistance(current, next);
+      }
+
+      current = next;
+    }
+
+    return new TspTour(data, tour, totalDistance);
   }
 }
