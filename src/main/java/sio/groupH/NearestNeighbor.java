81393b0d45ee6091f977932e7fe198a507cd016a
Hugo Huart <hugo.huart@heig-vd.ch>
2023-10-12 13:07:44 +0200

    misc: initial commit

---
+++ src/main/java/sio/groupH/NearestNeighbor.java
@@ -0,0 +1,13 @@
+package sio.groupH;
+
+import sio.tsp.TspData;
+import sio.tsp.TspConstructiveHeuristic;
+import sio.tsp.TspTour;
+
+public final class NearestNeighbor implements TspConstructiveHeuristic {
+  @Override
+  public TspTour computeTour(TspData data, int startCityIndex) {
+    // TODO
+    return null;
+  }
+}
