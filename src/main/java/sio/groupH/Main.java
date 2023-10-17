44003768529e659ebdd14850514b102e2d2d791c
Hugo Huart <hugo.huart@heig-vd.ch>
2023-10-17 15:55:27 +0200

    stats: initial stats display

--- src/main/java/sio/groupH/Main.java
+++ src/main/java/sio/groupH/Main.java
@@ -1,11 +1,79 @@
 package sio.groupH;
 
+import sio.tsp.TspConstructiveHeuristic;
 import sio.tsp.TspData;
 import sio.tsp.TspTour;
 
+import java.util.Arrays;
+
 public final class Main {
-  private String getStats(TspTour tour) {
-    return "";
+  private static String formatStatsForTours(TspTour[] tours, int optimalLength) {
+    if (tours == null || tours.length == 0) {
+      return "";
+    }
+
+    StringBuilder sb = new StringBuilder();
+
+    long minLength = tours[0].length();
+    int minIdx = 0;
+
+    long maxLength = minLength;
+    int maxIdx = 0;
+
+    double averageLength = 0.0;
+
+    long minNew, maxNew;
+    for (int i = 0; i < tours.length; ++i) {
+      minNew = Math.min(minLength, tours[i].length());
+      maxNew = Math.max(maxLength, tours[i].length());
+
+      if (minNew != minLength) {
+        minLength = minNew;
+        minIdx = i;
+      }
+
+      if (maxNew != maxLength) {
+        maxLength = maxNew;
+        maxIdx = i;
+      }
+
+      averageLength += tours[i].length() * (1.0 / tours.length);
+    }
+
+    sb.append("====\n");
+    sb.append(String.format("min length: %d started from %d\n", minLength, minIdx));
+    sb.append(String.format("diff from optimal (%d): %d%%\n", optimalLength, Math.round(100 * (double) minLength / optimalLength)));
+
+    sb.append("\n");
+    sb.append(String.format("max length: %d started from %d\n", maxLength, maxIdx));
+    sb.append(String.format("diff from optimal (%d): %d%%\n", optimalLength, Math.round(100 * (double) maxLength / optimalLength)));
+
+    sb.append("\n");
+    sb.append(String.format("average length: %d\n", Math.round(averageLength)));
+    sb.append(String.format("diff from optimal (%d): %d%%\n", optimalLength, Math.round(100 * averageLength / optimalLength)));
+
+    sb.append("====\n");
+
+    return sb.toString();
+  }
+
+  private static TspTour[] computeToursFromFile(String file, TspConstructiveHeuristic heuristic) {
+    TspData data;
+
+    try {
+      data = TspData.fromFile(file);
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+      return null;
+    }
+
+    TspTour[] tours = new TspTour[data.getNumberOfCities()];
+
+    for (int i = 0; i < data.getNumberOfCities(); ++i) {
+      tours[i] = heuristic.computeTour(data, i);
+    }
+
+    return tours;
   }
 
   public static void main(String[] args) {
@@ -27,7 +95,7 @@ public final class Main {
     // Exemple de lecture d'un jeu de données :
     // TspData data = TspData.fromFile("data/att532.dat");
 
-    String[] dataFiles = new String[] {
+    String[] dataFiles = new String[]{
         "data/att532.dat",
         "data/rat575.dat",
         "data/rl1889.dat",
@@ -36,15 +104,21 @@ public final class Main {
         "data/vm1748.dat"
     };
 
-    try {
-      for (String file : dataFiles) {
-        TspData data = TspData.fromFile(file);
+    int[] optimalLengths = new int[]{
+        86729,
+        6773,
+        316536,
+        36905,
+        57201,
+        336556
+    };
 
-        TspTour tour  = new NearestNeighbor().computeTour(data, 0);
-        System.out.printf("%s -> NN: %d\n", file, tour.length());
-      }
-    } catch(Exception e) {
-      System.out.println(e.getMessage());
+    for (int i = 0; i < dataFiles.length; ++i) {
+      TspTour[] tours = computeToursFromFile(dataFiles[i], new NearestNeighbor());
+      String stats = formatStatsForTours(tours, optimalLengths[i]);
+
+      System.out.println(dataFiles[i]);
+      System.out.println(stats);
     }
   }
 }
